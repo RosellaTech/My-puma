65222cc48aeceb0175eb08d411c25530d6b1d137
Rosella <rosellaecheruo1@gmail.com>
2025-04-12 17:12:25 +0100

    third Commit

--- src/components/trendingone/Trendingone.jsx
+++ src/components/trendingone/Trendingone.jsx
@@ -3,12 +3,12 @@ import "./Trendingone.css";
 const Trendingone = () => {
   return (
     <section>
-      <div className="text-center mt-10 mb-2">
+      <div className="text-center mt-7 mb-2 my-5  lg:my-2 lg:mt-8 text-[25px]">
         <h2>REWRITE THE CLASSICS</h2>
       </div>
       <div className="lg:flex flex-row tone-wrapper">
 
-        <div className="text-center tone-content">
+        <div className="text-center tone-content mb-5 lg:mb-0">
           <div className="tone-image">
             <img src="/images/png2.avif" alt="" />
           </div>
@@ -19,7 +19,7 @@ const Trendingone = () => {
           </div>
         </div>
 
-        <div className="tone-content">
+        <div className="tone-content mb-5 lg:mb-0">
           <div className="tone-image">
             <img src="/images/png3.avif" alt="" />
           </div>
--- src/components/trendingtwo/TrendingTwo.jsx
+++ src/components/trendingtwo/TrendingTwo.jsx
@@ -15,7 +15,7 @@ const TrendingTwo = () => {
   />
 
   {/* Left-aligned Text on Image */}
-  <div className="absolute top-1/2 left-8 transform -translate-y-1/2 text-white">
+  <div className=" absolute top-1/2 left-8 transform -translate-y-1/2 text-white">
     <h1 className="lg:text-4xl font-bold">UNLEASH UNLIMITED</h1>
     <p className="lg:text-lg mt-2">WITH FUTURE, ULTRA & KING</p>
 
@@ -29,11 +29,12 @@ const TrendingTwo = () => {
 
 
     <div className="text-center ">
-      <h2 className='mb-2'>HIGHLIGHTS</h2>
+      <h2 className=' mb-2 lg:mb-2 text-[25px]'>HIGHLIGHTS</h2>
     </div>
-    <div className="lg:flex flex-row tone-wrapper">
 
-      <div className="text-center tone-content mb-10">
+    <div className="lg:flex flex-row tone-wrapperr">
+
+      <div className="text-center tone-content mb-5 lg:mb-0">
         <div className="tone-image">
           <img src="/images/players.avif" alt="" />
         </div>
@@ -43,16 +44,16 @@ const TrendingTwo = () => {
         </div>
       </div>
 
-      <div className="tone-content">
+      <div className="mt-0 tone-content mb-5 lg:mb-0">
         <div className="tone-image">
           <img src="/images/shoe2.avif" alt="" />
         </div>
-        <div>
+        <div className=''>
           <h1 className="text-center">FENTY x PUMA</h1>
         </div>
       </div>
 
-      <div className="tone-content mt-6 lg:mt-0">
+      <div className="tone-content lg:mt-0">
         <div className="tone-image">
           <img src="/images/shoe3.avif" alt="" />
         </div>
